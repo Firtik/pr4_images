c576a5c8d5e977aeb4f459325340d64f00158950
mobile <mobile@iMac-PHTT.local>
2020-11-13 15:54:23 +0500

    Mfg

---
+++ app/src/main/java/com/example/myfirstgame/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myfirstgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonPlay = (Button)findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        i = new Intent(this, GameActivity.class);
+        startActivity(i);
+    }
+}
